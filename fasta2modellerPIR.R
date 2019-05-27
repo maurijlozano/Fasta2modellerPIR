9b14513686efefbf7964b62931d61c53ac6d55f9
Mauricio J. Lozano <maurijlozano@gmail.com>
2019-05-27 18:25:06 -0300

    Add files via upload

---
+++ fasta2modellerPIR.R
@@ -0,0 +1,81 @@
+#!/usr/bin/env Rscript
+# run with Rscript --vanilla scriptName.R
+
+args = commandArgs(trailingOnly=TRUE)
+args <- args[which(args!="")]
+
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} 
+
+
+pdb <- args[1]
+seqpdb <- args[2]
+ali <- args[3]
+seqname <- args[4]
+
+# test if there is at least one argument: if not, return an error
+
+#read talbe 8:length son missing residues
+pdbinfo <- read.table(pdb, header=FALSE, sep=" ", stringsAsFactors=FALSE)
+missing <- as.vector(unlist(pdbinfo[10:length(pdbinfo)]))
+
+#read seq from pdb
+pdbseq <- read.table(seqpdb, header=FALSE, sep=" ", stringsAsFactors=FALSE, colClasses = c("character"))
+pdbseq[missing] <- "-"
+
+#read alignment
+aliseqs <- read.table(ali, header=FALSE, sep=" ", stringsAsFactors=FALSE, colClasses = c("character"))
+seqname1<-aliseqs[grep(seqname,aliseqs[,1]),1]
+seqname2<-aliseqs[grep(seqname,aliseqs[,1],invert=T),1]
+
+
+
+seq1 <- read.fwf(textConnection(aliseqs[grep(seqname,aliseqs[,1]),2]), widths = rep(1, ceiling(max(nchar(aliseqs[1,2]) / 1))), stringsAsFactors = FALSE,colClasses = c("character"))
+seq2 <- read.fwf(textConnection(aliseqs[grep(seqname,aliseqs[,1],invert=T),2]), widths = rep(1, ceiling(max(nchar(aliseqs[2,2]) / 1))), stringsAsFactors = FALSE,colClasses = c("character"))
+
+#Transform alignment
+seq2gaps <- grep('-',seq2)
+
+newseq<-NULL
+#seq2 indexes
+pa<-1 #last position
+
+for (k in 1:length(seq2gaps)){
+	ngaps <- k-1
+	#seq2 indexes
+	i <- seq2gaps[k] - 1 #the position before the gap on seq2
+	if(seq2[i]!="-"){
+		#pdbseq indexes
+		ic <- i-ngaps #possition before the gap in pdbseq
+		pac <- pa # pa-ngaps-ncg
+		newseq <- c(newseq,as.vector(unlist(pdbseq[pac:ic])),"-")
+		ncg <- 0
+		pa<-ic+1 #last position after adding the gap on seq2
+	} else {
+		newseq <- c(newseq,"-")
+	}
+}
+newseq <- c(newseq,as.vector(unlist(pdbseq[(ic+1):length(pdbseq)])))
+newseq <- t(data.frame(newseq))
+#info for PIR alignment
+first <- min(grep('-',newseq,invert=T))
+lenstruct <- length(grep('-',newseq,invert=T))
+
+fn<-gsub('.fastab','.PIR',ali)
+#Write sequence
+write.table(paste('c; Converted with Fasta2modelerPIR, by M.J.Lozano'), fn, row.names = F, col.names = F,append=F,quote=F)
+write.table(paste('>P1;',seqname1,sep=''), fn, row.names = F, col.names = F,append=T,quote=F)
+write.table(paste('sequence:',seqname1,':     : :     : ::: 0.00: 0.00',sep=''), fn, row.names = F, col.names = F,append=T,quote=F)
+write.table(cbind(seq1[1,],"*"), fn, row.names = F, sep='', col.names = F,append=T,quote=F)
+
+#Write structure
+write.table(paste('>P1;',seqname2,"A",sep=''), fn, row.names = F, col.names = F,append=T,quote=F)
+write.table(paste('structureX:',seqname2,'.pdb:',first,'  :A:+',lenstruct,' :A:',pdbinfo[7],':: 0.00: 0.00',sep=''), fn, row.names = F, col.names = F,append=T,quote=F)
+write.table(cbind(newseq,"*"), fn, row.names = F, sep='', col.names = F,append=T,quote=F)
+
+
+
+
+
+
---
+++ fasta2modellerPIR.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+ALI=0
+PDB=0
+CHAIN=0
+SEQ=""
+show_help()
+{
+echo "
+        Usage: cCvsE.sh [-a] [-p] [-c] [-s]
+        
+        -h Show this Help
+        -a fasta alignment file
+        -p PDB file
+        -s Sequence iD
+        -c chain
+"
+
+echo -e '\nConverts fasta alignment to modeller PIR file. Only for One chain which must be specified.\n\n'
+
+exit 1
+}
+
+while getopts ":ha:s:c:p:" option; do
+    case "${option}" in
+        h) show_help
+            exit 1
+            ;;
+        a) ALI=$OPTARG;;
+        p) PDB=$OPTARG;;
+        c) CHAIN=$OPTARG;;
+        s) SEQ=$OPTARG;;
+        \?) echo "Invalid option: -${OPTARG}" >&2
+            exit 1
+            ;;
+        :)  echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Parameters required, run -h for help'
+    exit 1
+fi
+
+if [[ ${ALI} == 0 ]] ; then
+    echo 'Parameters -a alingment file required, run -h for help'
+    exit 1
+fi
+
+if [[ ${PDB} == 0 ]] ; then
+    echo 'Parameters -p, pdb file required, run -h for help'
+    exit 1
+fi
+
+if [[ ${CHAIN} == 0 ]] ; then
+    echo 'Using chain A, for other chains selection use -c argument'
+fi
+
+#Extracting missing residues
+cat "${PDB}" | grep -E 'REMARK 465     \w{3} \w     ?' | sed -E 's/REMARK 465     \w{3} \w     ?//' | sed -E 's/ //g' | tr '\n' ' ' | sed -E 's/ $//' > remark465.txted
+
+
+cat "${PDB}" | grep -E 'DBREF {1,5}[0-9A-Za-z]{1,5} '${CHAIN}'' |  sed -e 's/DBREF//' -e 's/^ *//' -e 's/ *$//' | sed -E 's/ {1,10}/ /g' > PDBSEQ.tab
+
+paste -d' ' PDBSEQ.tab remark465.txted > PDBSEQ.table
+rm PDBSEQ.tab remark465.txted 
+
+cat "${PDB}" | grep -E 'SEQRES {1,5}[0-9]{1,2} '${CHAIN}'' | sed -E 's/SEQRES   ?[0-9]{1,2} \w  [0-9]{1,3}  //' | sed -E 's/ {1,50}$//g'| tr '\n' ' ' | sed -e 's/MET/M/g' -e 's/PHE/F/g' -e 's/LEU/L/g' -e 's/ILE/I/g' -e 's/VAL/V/g' -e 's/SER/S/g' -e 's/PRO/P/g' -e 's/THR/T/g' -e 's/ALA/A/g' -e 's/TYR/Y/g' -e 's/HIS/H/g' -e 's/GLN/Q/g' -e 's/ASN/N/g' -e 's/ASP/D/g' -e 's/GLU/E/g' -e 's/CYS/C/g' -e 's/TRP/W/g' -e 's/ARG/R/g' -e 's/LYS/K/g' -e 's/GLY/G/g' -e 's/ $/\n/' > PDBSEQ_"${CHAIN}".seq
+
+
+#Processing fasta alingment
+cat "${ALI}" | perl -pe 'unless(/^>/){s/\n//g}; if(/>/){s/\n/ /g}; s/>/\n/' | sed '/^$/d' > "${ALI}tab"
+
+#Running R script
+Rscript --vanilla fasta2modellerPIR.R PDBSEQ.table PDBSEQ_"${CHAIN}".seq "${ALI}tab" "${SEQ}" &> /dev/null
+echo "Done"
+
